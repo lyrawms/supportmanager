751d6e2efb6a0a926ec07bbced4f2a1575667fdb
syndesiweb <65358781+syndesiweb@users.noreply.github.com>
2025-01-24 15:05:50 +0100

    Initial commit

---
+++ resources/js/Components/InputLabel.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+    value: String,
+});
+</script>
+
+<template>
+    <label class="text-base/6 text-zinc-950 select-none data-disabled:opacity-50 sm:text-sm/6 dark:text-white">
+        <span v-if="value">{{ value }}</span>
+        <span v-else><slot /></span>
+    </label>
+</template>
